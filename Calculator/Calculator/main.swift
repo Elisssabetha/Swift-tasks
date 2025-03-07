97bb78658c638bbb89996f06a5818454c5eee460
Liza Karankevich <karankevichelizaveta@gmail.com>
2025-03-08 01:50:33 +0300

    Калькулятор

--- Calculator/Calculator/main.swift
+++ Calculator/Calculator/main.swift
@@ -7,5 +7,27 @@
 
 import Foundation
 
-print("Hello, World!")
+func main() {
+    print("Выберите режим:")
+    print("1 - Калькулятор чисел")
+    print("2 - Калькулятор массива")
+    
+    while true {
+        if let userChoice = readLine(), let calc = Int(userChoice) {
+            switch calc {
+            case 1:
+                calculator()
+                return
+            case 2:
+                arrayOperations()
+                return
+            default:
+                print("Некорректный выбор. Введите 1 или 2.")
+            }
+        } else {
+            print("Некорректный ввод. Введите 1 или 2.")
+        }
+    }
+}
 
+main()
--- Calculator/Calculator/utils.swift
+++ Calculator/Calculator/utils.swift
@@ -6,3 +6,89 @@
 //
 
 import Foundation
+
+// расширение, чтобы преобразующее дабл в инт
+extension Double {
+    func formatNumber() -> Any {
+        if self == Double(Int(self)) {
+            return Int(self) // Если число целое, возвращаем как Int
+        } else {
+            return self // Иначе возвращаем как Double
+        }
+    }
+    // округление
+    func rounded(toPlaces places: Int) -> Double {
+            let divisor = pow(10.0, Double(places))
+            return (self * divisor).rounded() / divisor
+        }
+}
+
+struct MathOperations {
+    static func sum(_ a: Double, _ b: Double) -> Any {
+        return (a + b).formatNumber()
+    }
+    
+    static func subtract(_ a: Double, _ b: Double) -> Any {
+        return (a - b).formatNumber()
+    }
+    
+    static func multiply(_ a: Double, _ b: Double) -> Any {
+        return (a * b).formatNumber()
+    }
+    
+    static func divide(_ a: Double, _ b: Double) -> Any {
+        return (a / b).formatNumber()
+    }
+    
+    static func power(_ a: Double, _ b: Double) -> Any {
+        return pow(a, b).formatNumber()
+    }
+    
+    static func getRestDevided(_ a: Int, _ b: Int) -> Any {
+        return a % b
+    }
+}
+
+//обработка ввода мат операции
+func inputOperator(_ text: String) -> String {
+    print(text)
+    while true {
+        if let operation = readLine(), ["+", "-", "*", "/", "^", "%"].contains(operation) {
+            return operation
+        } else {
+            print("Вы ввели некорректный оператор, попробуйте снова.")
+        }
+    }
+}
+
+// обработка ввода пользователем чисел
+func input(_ text: String) -> Double {
+    print(text)
+    while true {
+        //раскрываем String?
+        if let userInput = readLine() {
+            // проверяем можно ли введенное преобразовать в дабл ( то есть число ли это)
+            if let number = Double(userInput) {
+                return number
+            } else {
+                print("Вы ввели не число, попробйте снова.")
+            }
+        } else {
+            print("Ошибка ввода.")
+        }
+    }
+}
+
+func inputArrayLength(_ text: String) -> Int {
+    while true {
+            let length = input(text) // здесь дабл
+            
+            // Переводим в инт, для сравнения обратно в дабл
+            if Double(Int(length)) == length && length > 0 {
+                return Int(length)
+            } else {
+                print("Длина массива должна быть положительным целым числом. Попробуйте снова.")
+            }
+        }
+    
+}
